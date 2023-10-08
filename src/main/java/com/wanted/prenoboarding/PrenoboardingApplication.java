cf67d99bf732f9298c0aaffb345ea489890be1f3
OhChaeYoung <oh5chaeyoung@gmail.com>
2023-10-08 22:43:58 +0900

    init: Init first commit

---
+++ src/main/java/com/wanted/prenoboarding/PrenoboardingApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.prenoboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenoboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenoboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/prenoboarding/PrenoboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.prenoboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenoboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
