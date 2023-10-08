5eb57baba8ba06f2e2d6b8745c6bf932596ab992
OhChaeYoung <oh5chaeyoung@gmail.com>
2023-10-08 23:28:53 +0900

    feat: Implement company exception

---
+++ src/main/java/com/wanted/prenoboarding/exception/CompanyNotFoundException.java
@@ -0,0 +1,8 @@
+package com.wanted.prenoboarding.exception;
+
+public class CompanyNotFoundException extends RuntimeException {
+
+	public CompanyNotFoundException(String message) {
+		super(message);
+	}
+}
