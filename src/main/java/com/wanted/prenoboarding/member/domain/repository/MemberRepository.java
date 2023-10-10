000e78231f3898c77a9660e280014ed138105118
OhChaeYoung <oh5chaeyoung@gmail.com>
2023-10-10 15:05:35 +0900

    feat: Implement member
    entity, repository

---
+++ src/main/java/com/wanted/prenoboarding/member/domain/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.wanted.prenoboarding.member.domain.repository;
+
+import com.wanted.prenoboarding.member.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
